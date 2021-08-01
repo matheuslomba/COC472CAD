7e2018d211a1bd07ef40c4811d60ddabc79d7638
Lomba <mathlomba@gmail.com>
2021-08-01 15:56:34 -0300

    V1

---
+++ Trabalho1/C/main.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//para buildar: gcc main.c -o run
+//para rodar: ./run
+
+//-------------------------------------------------------------------------------
+
+int multiplicaij (int ordemN, int **matrizA, int *vetorX) {
+
+    int *vetorB = (int *) malloc(ordemN * sizeof(int));
+    int i, j, b;
+
+    clock_t t;
+
+    printf("Primeira ordem de execução:\n");
+
+    t = clock();
+
+    for (i = 0; i < ordemN; i++) {
+        b = 0;
+        for (j = 0; j < ordemN; j++) {
+            
+            b += matrizA[i][j] * vetorX[j];
+
+        }
+        vetorB[i] = b;
+    }
+
+    //Printa o resultado vetorB, mas coloquei como comentário visto o tamanho dos arrays
+    /*
+    printf("Vetor B: [ ");
+    for (int i = 0; i < ordemN; i++) {
+        printf("%d ", vetorB[i]);
+    }
+    printf("]\n");
+    */
+
+    t = clock() - t;
+    printf("Tempo de execução: %lf segundos.\n", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversão do tempo para double
+    
+    //Liberar memória
+    for (i = 0; i < ordemN; i++) {
+        free(matrizA[i]);
+    }
+    free(matrizA);
+    free(vetorX);
+    free(vetorB);
+    
+    return 0;
+}
+
+//-------------------------------------------------------------------------------
+
+int multiplicaji (int ordemN, int **matrizA, int *vetorX) {
+
+    int *vetorB = (int *) malloc(ordemN * sizeof(int));
+    int i, j, b;
+
+    clock_t t;
+
+    printf("Segunda ordem de execução:\n");
+
+    t = clock();
+
+    for (i = ordemN-1; i >= 0; i--) {
+        b = 0;
+        for (j = ordemN-1; j >= 0; j--) {
+            
+            b += matrizA[i][j] * vetorX[j];
+
+        }
+        vetorB[i] = b;
+    }
+
+    //Printa o resultado vetorB, mas coloquei como comentário visto o tamanho dos arrays
+    /*
+    printf("Vetor B: [ ");
+    for (int i = 0; i < ordemN; i++) {
+        printf("%d ", vetorB[i]);
+    }
+    printf("]\n");
+    */
+
+    t = clock() - t;
+    printf("Tempo de execução: %lf segundos.\n", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversão do tempo para double
+    
+    //Liberar memória
+    //Liberar memória
+    for (i = 0; i < ordemN; i++) {
+        free(matrizA[i]);
+    }
+    free(matrizA);
+    free(vetorX);
+    free(vetorB);
+
+    return 0;
+}
+
+//-------------------------------------------------------------------------------
+
+int main () {
+
+    int *vetorX;
+    int **matrizA;
+    int i, j, ordemN;
+
+    srand(time(NULL));
+
+    for (int max = 0; max <= 2300; max = max+100) {
+        
+        printf("Iteração %d\n", 1+(max/100));
+        ordemN = (rand() %101) + max;
+        printf("Tamanho dos arrays: %d\n", ordemN);
+        printf("-------------------------------\n");
+
+        //Alocação de ponteiros para criar os vetores e a matriz
+        vetorX = (int *) malloc(ordemN * sizeof(int));
+        matrizA = (int **) malloc(ordemN * sizeof(int *));
+        for (i = 0; i < ordemN; i++) {
+            matrizA[i] = (int *) malloc(ordemN * sizeof(int));
+            for (j = 0; j < ordemN; j++) {
+                matrizA[i][j] = rand() %100;
+            }
+            vetorX[i] = rand() %100;
+        }
+
+        /*
+        int matrizA[ordemN][ordemN];
+        int vetorX [ordemN];
+
+        for (int i = 0; i < ordemN; i++) {
+            for (int j = 0; j < ordemN; j++) {
+                matrizA[i][j] = rand() %100;
+            }
+            vetorX[i] = rand() %100;
+        }*/
+
+        //Printa matrizA e vetorX, mas coloquei como comentário visto o tamanho dos arrays
+        /*
+        printf("Matriz A:\n");
+        for (int i = 0; i < ordemN; i++) {
+            for (int j = 0; j < ordemN; j++) {
+
+                printf("%d ", matrizA[i][j]);
+
+            }
+            printf("\n");
+        }
+
+        printf("Vetor X: [ ");
+        for (int i = 0; i < ordemN; i++) {
+
+            printf("%d ", vetorX[i]);
+        
+        }
+        printf("]\n"); 
+        printf("-------------------------------\n");
+        */
+
+        multiplicaij(ordemN, matrizA, vetorX);
+        printf("-------------------------------\n");
+        multiplicaji(ordemN, matrizA, vetorX);
+        printf("------------------------------------------------------------\n");
+
+        //Liberar memória
+        for (i = 0; i < ordemN; i++) {
+            free(matrizA[i]);
+        }
+        free(matrizA);
+        free(vetorX);
+        /*for (i = 0; i < ordemN; i++) {
+            free(vetorX[i]);
+            for (j = 0; j < ordemN; j++) {
+                free(matrizA[i][j]);
+            }
+            free(matrizA[i]);
+        }
+        free(matrizA);
+        free(vetorX);*/
+
+    }
+}
+
---
+++ Trabalho1/Fortran/main.f95
@@ -0,0 +1,79 @@
+! compilador: gfortran main.f95
+! ./a.out
+
+!-------------------------------------------------------------------
+
+program main
+
+    implicit none
+    integer :: maximo, ordem, i, j, b
+    real :: ordemN, tij_i, tij_f, tji_i, tji_f
+
+    real, dimension(:,:), allocatable :: matrizA
+    real, dimension(:), allocatable :: vetorX
+    real, dimension(:), allocatable :: vetorB
+
+    do maximo = 0, 44 !maximo do máximo = 45000
+
+        print*, "--------------------------------------------------------------------"
+
+        call random_number(ordemN)
+        ordem = int(ordemN*1000) + maximo*1000
+
+        print*, "Tamanho dos arrays: ", ordem
+        print*, "--------------------------------"
+
+        if (allocated(matrizA)) then
+            deallocate(matrizA)
+        endif
+        if (allocated(vetorX)) then
+            deallocate(vetorX)
+        endif
+        if (allocated(vetorB)) then
+            deallocate(vetorB)
+        endif
+        allocate(matrizA(ordem, ordem))
+        allocate(vetorX(ordem))
+        allocate(vetorB(ordem))
+
+        call random_seed
+        call random_number(matrizA)
+        call random_number(vetorX)
+
+        ! Multiplicaij
+        print*, "Primeira ordem de execução:"
+        call cpu_time(tij_i)
+        do i = 1, ordem
+            b = 0
+            do j = 1, ordem
+
+                b = b + matrizA(i, j)*10 * vetorX(j)*10
+
+            end do
+            vetorB(i) = b
+
+        end do
+        call cpu_time(tij_f)
+        print*, "Tempo de execução: ", tij_f-tij_i, " segundos."
+    
+        ! Multiplicaji
+        print*, "Segunda ordem de execução:"
+        call cpu_time(tji_i)
+        do i = ordem, 1
+            b = 0
+            do j = ordem, 1
+
+                b = b + matrizA(i, j)*10 * vetorX(j)*10
+
+            end do
+            vetorB(i) = b
+
+        end do
+        call cpu_time(tji_f)
+        print*, "Tempo de execução: ", tji_f-tji_i, " segundos."
+
+    end do
+
+end program main
+
+
